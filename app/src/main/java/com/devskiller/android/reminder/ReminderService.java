790d7f1a5020c17ad0603679cce156f120dd1277
Sourabh <sourabh.shegane@flytbase.com>
2022-06-16 00:39:30 +0530

    Feat: Schedule reminder

--- app/src/main/java/com/devskiller/android/reminder/ReminderService.java
+++ app/src/main/java/com/devskiller/android/reminder/ReminderService.java
@@ -1,16 +1,33 @@
 package com.devskiller.android.reminder;
 
+import static android.content.Context.ALARM_SERVICE;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 
+import com.devskiller.android.reminder.broadcast_receivers.AlarmBroadcastReceiver;
+
 public class ReminderService {
+    private Context context;
+    private ReminderRepository reminderRepository;
+
     public ReminderService(@NonNull Context context, @NonNull ReminderRepository reminderRepository) {
-        // SOLUTION
+        this.context = context;
+        this.reminderRepository = reminderRepository;
     }
 
     public void scheduleReminder(@NonNull Reminder reminder) {
         // SOLUTION
+        Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, reminder.getId(), intent, 0);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, reminder.getWhen(), pendingIntent);
+
+       reminderRepository.save(reminder);
     }
 
     public void markDone(@NonNull Reminder reminder) {
---
+++ app/src/main/java/com/devskiller/android/reminder/broadcast_receivers/AlarmBroadcastReceiver.kt
@@ -0,0 +1,12 @@
+package com.devskiller.android.reminder.broadcast_receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmBroadcastReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        //TODO: Create a new notification here
+    }
+}
