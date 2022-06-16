c3e60020223a694ba085b20e8965f6b8ff45503a
Sourabh <sourabh.shegane@flytbase.com>
2022-06-16 20:51:29 +0530

    Refactor: Get data and close notification when clicked on mark as done notification action

--- app/src/main/java/com/devskiller/android/reminder/broadcast_receivers/NotificationActionReceiver.kt
+++ app/src/main/java/com/devskiller/android/reminder/broadcast_receivers/NotificationActionReceiver.kt
@@ -3,12 +3,22 @@ package com.devskiller.android.reminder.broadcast_receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import androidx.core.app.NotificationManagerCompat
 import com.devskiller.android.reminder.Reminder
 
 class NotificationActionReceiver: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val reminderToBeMarkedAsDone = intent?.getSerializableExtra("reminder") as Reminder
-        //TODO Update the id of this reminder as done
+
+        context?.let {
+            intent?.let {
+                val reminderBundle = intent.getParcelableExtra<Bundle>("reminder")
+                val reminder = reminderBundle?.getSerializable("reminder") as Reminder?
+                reminder?.let {
+                    NotificationManagerCompat.from(context).cancel(null, reminder.id)
+                }
+            }
+        }
     }
 }
