b97ef62fc7743be91ab265b6c2a65548176f7915
Sourabh <sourabh.shegane@flytbase.com>
2022-06-16 13:24:31 +0530

    Refactor: Convert Java POJO to Kotlin data class

--- app/src/main/java/com/devskiller/android/reminder/Reminder.kt
+++ app/src/main/java/com/devskiller/android/reminder/Reminder.kt
@@ -1,53 +1,12 @@
-package com.devskiller.android.reminder;
+package com.devskiller.android.reminder
 
-import androidx.annotation.NonNull;
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.io.Serializable
 
-import java.io.Serializable;
-
-public class Reminder implements Serializable {
-    private int id;
-    private String title;
-    private boolean done;
-    private long when;
-
-    public Reminder(int id, @NonNull String title, long when) {
-        this(title, when);
-        this.id = id;
-    }
-
-    public Reminder(@NonNull String title, long when) {
-        this.title = title;
-        this.when = when;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Reminder setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    public Reminder setDone(boolean done) {
-        this.done = done;
-        return this;
-    }
-
-    public long getWhen() {
-        return when;
-    }
-
-    public Reminder setWhen(long when) {
-        this.when = when;
-        return this;
-    }
-
-    public int getId() {
-        return id;
-    }
-}
+data class Reminder(
+    val id: Int = 0,
+    val title: String,
+    val done: Boolean = false,
+    val time: Long = 0L
+): Serializable
