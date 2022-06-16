fcd59f54d0ad2c99b43162923023dfaf13321eec
Sourabh <sourabh.shegane@flytbase.com>
2022-06-16 13:25:19 +0530

    Refactor: Get reminder data from bundle

--- app/src/main/java/com/devskiller/android/reminder/ReminderNotificationService.java
+++ app/src/main/java/com/devskiller/android/reminder/ReminderNotificationService.java
@@ -6,6 +6,8 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
@@ -14,19 +16,26 @@ import androidx.core.app.NotificationManagerCompat;
 import com.devskiller.android.reminder.broadcast_receivers.NotificationActionReceiver;
 
 public class ReminderNotificationService extends IntentService {
+
+    private final String TAG = "ReminderNotification";
+
     public ReminderNotificationService() {
         super(ReminderNotificationService.class.getSimpleName());
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        Reminder reminder = (Reminder) intent.getSerializableExtra("reminder");
+        Bundle reminderBundle = intent.getParcelableExtra("reminder");
+        Reminder reminder = (Reminder) reminderBundle.getSerializable("reminder");
         if(reminder != null){
             createNewNotification(reminder);
+        }else{
+            Log.d(TAG, "onHandleIntent: Reminder is null");
         }
     }
 
     private void createNewNotification(Reminder reminder) {
+        Log.d(TAG, "Creating new notification");
         createNotificationChannel();
 
         Intent intent = new Intent(this, ReminderDetailsActivity.class);
@@ -40,7 +49,7 @@ public class ReminderNotificationService extends IntentService {
                 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "ZOCKET_REMINDER_SERVICE")
                 .setSmallIcon(R.drawable.abc_vector_test)
                 .setContentTitle(reminder.getTitle())
                 .setContentText(reminder.getTitle())
@@ -48,8 +57,11 @@ public class ReminderNotificationService extends IntentService {
                 .addAction(R.mipmap.ic_launcher, "Mark as Done", actionIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+
+        Log.d(TAG, "Firing new notification...");
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(reminder.getId(), builder.build());
+
     }
 
     private void createNotificationChannel() {
